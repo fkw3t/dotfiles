5e8f237410357ce5226cfa15e5de748733441bf4
Eliabner Teixeira <46829134+fkw3t@users.noreply.github.com>
2025-07-19 16:02:50 -0300

    chore(nvim): adjustments

--- .config/nvim/lua/eliabnertxmq/plugins/colors.lua
+++ .config/nvim/lua/eliabnertxmq/plugins/colors.lua
@@ -1,82 +1,97 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-				-- styles = {
-				-- 	italic = false,
-				-- },
-				styles = {
-					comments = { "italic" },
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = { "bold" },
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = { "bold" },
-					properties = {},
-					types = { "bold" },
-					operators = {},
-				},
-			})
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                -- styles = {
+                -- 	italic = false,
+                -- },
+                styles = {
+                    comments = { "italic" },
+                    conditionals = { "italic" },
+                    loops = {},
+                    functions = {},
+                    keywords = { "bold" },
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = { "bold" },
+                    properties = {},
+                    types = { "bold" },
+                    operators = {},
+                },
+            })
 
-			-- vim.cmd.colorscheme("rose-pine")
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		as = "catppuccin",
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-				styles = {
-					comments = { "italic" },
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = { "bold" },
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = { "bold" },
-					properties = {},
-					types = { "bold" },
-					operators = {},
-				},
-			})
-			-- vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	-- {
-	-- 	"kdheepak/monochrome.nvim",
-	-- 	as = "monochrome",
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("monochrome")
-	-- 	end,
-	-- },
-	{
-		"slugbyte/lackluster.nvim",
-		config = function()
-			local lackluster = require("lackluster")
-			require("nvim-web-devicons").setup({
-				color_icons = false,
-				override = {
-					["default_icon"] = {
-						color = lackluster.color.gray4,
-						name = "Default",
-					},
-				},
-			})
+            -- vim.cmd.colorscheme("rose-pine")
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        as = "catppuccin",
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true,
+                styles = {
+                    comments = { "italic" },
+                    conditionals = { "italic" },
+                    loops = {},
+                    functions = {},
+                    keywords = { "bold" },
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = { "bold" },
+                    properties = {},
+                    types = { "bold" },
+                    operators = {},
+                },
+            })
+            -- vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+    {
+        "slugbyte/lackluster.nvim",
+        config = function()
+            local lackluster = require("lackluster")
+            lackluster.setup({
+                disable_plugin = {},
+                tweak_highlight = {
+                    ["@keyword"] = {
+                        overwrite = true,
+                        bold = true,
+                    },
+                    ["@function"] = {
+                        overwrite = true,
+                        bold = true,
+                    },
+                    ["@boolean"] = {
+                        overwrite = true,
+                        bold = true,
+                    },
+                    ["@type"] = {
+                        overwrite = true,
+                        bold = true,
+                    },
+                },
+            })
 
-			-- vim.cmd.colorscheme("lackluster")
-			-- vim.cmd.colorscheme("lackluster-hack")
-			vim.cmd.colorscheme("lackluster-mint")
-			-- vim.cmd.colorscheme("lackluster-dark")
-		end,
-	},
+            require("nvim-web-devicons").setup({
+                color_icons = false,
+                override = {
+                    ["default_icon"] = {
+                        color = lackluster.color.gray6,
+                        name = "Default",
+                    },
+                },
+            })
+
+            -- vim.cmd.colorscheme("lackluster")
+            -- vim.cmd.colorscheme("lackluster-hack")
+            -- vim.cmd.colorscheme("lackluster-mint")
+            vim.cmd.colorscheme("lackluster-dark")
+        end,
+    },
 }
--- .config/nvim/lua/eliabnertxmq/plugins/treesitter.lua
+++ .config/nvim/lua/eliabnertxmq/plugins/treesitter.lua
@@ -1,48 +1,51 @@
+---@diagnostic disable: missing-fields
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			-- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"yaml",
-				"markdown",
-				"bash",
-				"lua",
-				"php",
-				"go",
-				"python",
-				"sql",
-				"dockerfile",
-				"terraform",
-				"gitignore",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-	end,
+        treesitter.setup({
+            -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enable = true,
+            },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "jsonc",
+                "yaml",
+                "toml",
+                "markdown",
+                "bash",
+                "lua",
+                "php",
+                "go",
+                "python",
+                "sql",
+                "dockerfile",
+                "terraform",
+                "gitignore",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
 }
